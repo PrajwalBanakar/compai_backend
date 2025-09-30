10d41735a924c4f86c2942a8d95baac267452439
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2025-09-30 17:56:45 -0400

    added prismaservice to sessions module

--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -4,10 +4,11 @@ import { SessionsController } from './sessions.controller';
 import { SessionsService } from './sessions.service';
 import { AiModule } from 'src/ai/ai.module';
 import { AIService } from 'src/ai/ai.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   imports: [AiModule],
   controllers: [SessionsController],
-  providers: [SessionsService, AIService]
+  providers: [SessionsService, AIService, PrismaService],
 })
 export class SessionsModule {}
