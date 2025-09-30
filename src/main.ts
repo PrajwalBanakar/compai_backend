fa49c84241401aa8cd2abfec3f569cbed2bce810
Prajwal Banakar <prajwalbanakar040999@gmail.com>
2025-09-30 17:52:52 -0400

    configured nestJS for Render Deployment

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: 'http://localhost:3000', // allow frontend origin
+    origin: ['https://compai-frontend.vercel.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 4000);
-  console.log('Backend running on http://localhost:4000');
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Backend running on http://localhost:${port}`);
 }
 void bootstrap();
